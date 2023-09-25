568801b0db67018c70d6c357d6aad932091aaa0f
Ray Fochler <rrgunsite@gmail.com>
2023-09-25 18:34:18 -0400

    Update pingsweep.sh

--- pingsweep.sh
+++ pingsweep.sh
@@ -5,9 +5,10 @@ if [ "$1" == "" ]
 then
 echo "You forgot an IP address!"
 echo "Syntax: ./ipsweep.sh 192.168.1"
-
+#for loop ping with 1 count to check if host is up
 else
 for ip in `seq 1 254`; do
+#cut ouput to only return last octet
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
 fi
